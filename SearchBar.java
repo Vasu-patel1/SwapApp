3e9414ebe179ca5eb767c51d1b19cea18f513156
Vasu-patel1 <91266677+Vasu-patel1@users.noreply.github.com>
2022-02-01 17:42:34 -0500

    Add files via upload

---
+++ SearchBar.java
@@ -0,0 +1,14 @@
+package com.example.swapswop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SearchBar extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.SearchBar);
+    }
+}
